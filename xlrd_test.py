2c6abf24ca303127e6a042413a3253ce26db14a1
tobybellwood <toby.bellwood@gmail.com>
2014-05-12 12:06:50 +0000

    prettytables version

--- xlrd_test.py
+++ xlrd_test.py
@@ -1,43 +1,39 @@
 import urllib
 import xlrd
-from xlrd.timemachine import REPR
+from xlutils.display import cell_display
+from prettytable import PrettyTable
+from xlrd.sheet import Cell
 
-def showable_cell_value(celltype, cellvalue, datemode):
-    if celltype == xlrd.XL_CELL_DATE:
-        try:
-            showval = xlrd.xldate_as_tuple(cellvalue, datemode)
-        except xlrd.XLDateError as e:
-            showval = "%s:%s" % (type(e).__name__, e)
-    elif celltype == xlrd.XL_CELL_ERROR:
-        showval = xlrd.error_text_from_code.get(
-            cellvalue, '<Unknown error code 0x%02x>' % cellvalue)
-    else:
-        showval = cellvalue
-    return showval
-
-dataset = 'A2519058W'
+dataset = 'A2420643K'
 filename = urllib.urlretrieve("http://www.ausstats.abs.gov.au/ausstats/ABS@Archive.nsf/GetTimeSeries?OpenAgent&sid="+dataset, '/tmp/'+dataset+'.xls')
 book = xlrd.open_workbook('/tmp/'+dataset+'.xls')
+datemode = book.datemode
+dataset_dict = {}
 for nobj in book.name_obj_list:
     name = nobj.name
-    if name == dataset:
+    datasetRange = dataset+'_Data'
+    if name == datasetRange: #or name == 'Date_Range':
         res = nobj.result
         kind = res.kind
         value = res.value
-        print name,res,kind,value
         for i in range(len(value)):
             ref3d = value[i]
-            print("Range %d: %s ==> %s"% (i, REPR(ref3d.coords), REPR(xlrd.rangename3d(book, ref3d))))
-            datemode = book.datemode
             for shx in range(ref3d.shtxlo, ref3d.shtxhi):
                 sh = book.sheet_by_index(shx)
-                print("   Sheet #%d (%s)" % (shx, sh.name))
-                rowlim = min(ref3d.rowxhi, sh.nrows)
-                collim = min(ref3d.colxhi, sh.ncols)
-                for rowx in range(ref3d.rowxlo, rowlim):
-                    for colx in range(ref3d.colxlo, collim):
+                for rowx in range(ref3d.rowxlo, ref3d.rowxhi):
+                    for colx in range(ref3d.colxlo, ref3d.colxhi):
                         cty = sh.cell_type(rowx, colx)
                         cval = sh.cell_value(rowx, colx)
-                        sval = showable_cell_value(cty, cval, datemode)
-                        print("      (%3d,%3d) %-5s: %s"
-                            % (rowx, colx, xlrd.cellname(rowx, colx), REPR(sval)))
+                        cdate = sh.cell_value(rowx, 0)
+                        if cty == 3:
+                            cval = cell_display(Cell(cty, cval))
+                            #sval = xlrd.xldate_as_tuple(cval,datemode)
+                        dataset_dict[xlrd.xldate_as_tuple(cdate, datemode)] = cval
+x = PrettyTable()
+x.add_column("Date", dataset_dict.keys())
+x.add_column(dataset, dataset_dict.values())
+x.sortby = "Date"
+print x
+
+#for key, value in sorted(dataset_dict.items()):
+#    print("{} : {}".format(key, value))
