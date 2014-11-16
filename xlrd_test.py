09fa2c801ab969d8833c8f7dbb4eba791abfbf95
Toby Bellwood <toby.bellwood@gmail.com>
2014-11-16 12:58:43 +1100

    Updated to Python3

--- xlrd_test.py
+++ xlrd_test.py
@@ -1,35 +1,41 @@
+ #!/usr/local/bin/python3.4
 import time
-import urllib
+import csv
+import urllib.request, urllib.parse, urllib.error
 import xlrd
 import pandas as pd
+import platform
 #from xlutils.display import cell_display
 #from xlrd.sheet import Cell
 
 timerStart = time.time()
-
-# Insert a comma seperated listof ABS Series IDs
-dataset_list = ('A163158W','A181518A','A2325846C')
-
 # build an empty Pandas dataframe to concatenate the retrieved data into
-outputSeries = pd.DataFrame()
+curMonth = int(time.strftime("%m"))
+curYear = int(time.strftime("%Y"))
 
-for dataset in dataset_list:
-    # Using the methods outlined in http://www.abs.gov.au/AUSSTATS/abs@.nsf/97adb482c0aba769ca2570460017d0e7/15d1d9943893067dca2572ec00157f40!OpenDocument
-    # append _Data to the dataset name to access only series data
-    datasetRange = dataset+'_Data'
-    # Call the most recent publication using the OpenAgent&sid= string and download it to tmp drive
-    filename = urllib.urlretrieve("http://www.ausstats.abs.gov.au/ausstats/Meisubs.nsf/GetTimeSeries?OpenAgent&sid="+dataset, './'+dataset+'.xls')
-    # use xlrd to open the workbook
-    book = xlrd.open_workbook('./'+dataset+'.xls')
+if curMonth > 6:
+    curYear += 1
+
+numberOfRows = ((curYear - 1900)*12)+6
+pd.options.display.float_format = '{:,.2f}'.format
+
+prng = pd.period_range('Jan-1900', periods=numberOfRows, freq='M')
+indexFrame = pd.DataFrame(index = prng)
+
+def fetchSeries(chapter, file, series):
+    book = xlrd.open_workbook(file)
     # important - set xlrd datemode to match the excel book - avoiding any 1900/1904 problems
     datemode = book.datemode
-    # build a temporary dictionary to hold the series data
-    dataset_dict = {}
+    # build a temporary Pandas Series to hold the data
+    s = pd.Series(name=chapter)
+    if series[0]=="A":
+        series = series + "_Data"
 
     # name_obj_list is a list of all named ranges in the excel book
     for nobj in book.name_obj_list:
         name = nobj.name
-        if name == datasetRange:
+        if name == series:
+            print (name)
             # result.value is the actual reference to the series (there can be multiple references - A1:A10,A18:A50 etc
             value = nobj.result.value
             for i in range(len(value)):
@@ -46,17 +52,35 @@ for dataset in dataset_list:
                             cval = sh.cell_value(rowx, colx)
                             # get the date that corresponds to the series entry (column 0 is the date column)
                             cdate = sh.cell_value(rowx, 0)
-                            # add the cell value to the dictionary, with the date tuple as the key
-                            dataset_dict[xlrd.xldate_as_tuple(cdate, datemode)] = cval
-    # build a temporary pandas dataframe with Date and dataset name columns
-    s = pd.DataFrame(dataset_dict.items(), columns=['Date', dataset])
-    # index the temporary dataframe on the date
-    s = s.set_index('Date')
-    # concatenate the temp dataframe to the master dataframe.  This will add a new column, creating new keys as required.
-    outputSeries = pd.concat([outputSeries, s], axis=1)
+                            # create a tuple holding the date
+                            date_tup = xlrd.xldate_as_tuple(cdate, datemode)
+                            # set the series value for this date
+                            s[pd.datetime(date_tup[0], date_tup[1], date_tup[2])] = cval
+    return s
 
-outputSeries.to_csv('./output.csv')
-print outputSeries
+
+links = set()
+
+with open('MSB_Sources.csv', 'Ur') as f:
+        datasources = list(tuple(rec) for rec in csv.reader(f, delimiter=','))
+for item in datasources:
+    links.add(tuple((item[1],item[2])))
+for link in links:
+    filename = urllib.request.urlretrieve(link[0], link[1])
+    for item in datasources:
+        if item[2] == link[1]:
+            print (item)
+            s = fetchSeries(item[0],item[2],item[3])
+            s = s.to_period(freq='M')
+            print (s)
+            indexFrame = pd.concat([indexFrame, s], axis=1)
+
+#indexFrame = indexFrame.groupby(indexFrame.index.month).sum()
+print (indexFrame)
+
+indexFrame.reindex_axis(sorted(indexFrame.columns), axis=1)
+indexFrame.to_csv('./output.csv')
 timerEnd = time.time()
 timerInterval = timerEnd - timerStart
-print "process took "+str(timerInterval)+" seconds"
+print("process took "+str(timerInterval)+" seconds")
+print((platform.python_version()))
